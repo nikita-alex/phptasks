816f86609024bf28e1be774ab76e864fc9104a07
Nikita.A <nikson491@gmail.com>
2023-07-19 22:40:29 +0600

    message

---
+++ Tests/NwqTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests;
+
+use App\Name;
+use PHPUnit\Framework\TestCase;
+
+class NwqTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testClassConstructor()
+    {
+        $name1 = new Name();
+        $name2 = new Name();
+
+        $this->assertEquals($name1, $name2);
+    }
+
+}
