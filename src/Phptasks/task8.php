6e8e07ec1621fb7b7ec5772ff2e29aa1b2ff0f0f
Nikita.A <nikson491@gmail.com>
2023-07-20 17:14:53 +0600

    task #8

--- src/Phptasks/task8.php
+++ src/Phptasks/task8.php
@@ -1,5 +1,19 @@
 <?php
 const m = 3;
 const n = 3;
+$i = 0;
+$final_array = [];
+while ($i < m):
+    $j = 0;
+    $temp_array = [];
+    while ($j < n):
+        array_push($temp_array, rand(0, 10));
+        $j++;
+    endwhile;
+    array_push($final_array, $temp_array);
+//    unset($temp_array);
+    $i++;
+endwhile;
+var_dump($final_array);
 
 
